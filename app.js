8c8d2bc1450fe55b4368fc6362881de072870e14
jbergant <jana.bergant@gmail.com>
2016-12-19 15:30:16 +0100

    Update app.js
    more than one message

--- app.js
+++ app.js
@@ -276,7 +276,7 @@ function handleApiAiResponse(sender, response) {
 
 	sendTypingOff(sender);
 
-	if (isDefined(messages)) {
+	if (isDefined(messages) && messages.length > 1) {
 		let timeoutInterval = 1100;
 		let previousType ;
 		let cardTypes = [];
