c3313f0e7064d991cf9cc36b8377b0b00cf33144
Ron Hagafny <Ronhagafny20@gmail.com>
2017-04-27 11:54:25 +0300

    Initial Commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'a',
+    FB_VERIFY_TOKEN: 'b',
+    API_AI_CLIENT_ACCESS_TOKEN: 'c',
+    FB_APP_SECRET: 'd',
+    SERVER_URL: "e",
 };
