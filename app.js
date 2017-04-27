e9aeb9bcaad81b0869d168c9cb9cc5a2a358317f
Ron Hagafny <Ronhagafny20@gmail.com>
2017-04-27 21:13:26 +0300

    removed authentication files

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const app = express();
 const crypto = require('crypto');
 const bodyParser = require('body-parser');
 const apiAiModule = require('./apiaiModule');
-const passport = require('passport');
-const createSendToken = require('./services/jwt.js');
-const facebookAuth = require('./services/facebookAuth.js');
-const localStrategy = require('./services/localStrategy.js');
 const routes = require('./routes');
 
 require('./dbConnector')(() => {
@@ -33,13 +29,6 @@ app.use(bodyParser.urlencoded({
 // Process application/json
 app.use(bodyParser.json());
 
-passport.serializeUser(function (user, done) {
-    done(null, user.id);
-});
-
-passport.use('local-register', localStrategy.register);
-passport.use('local-login', localStrategy.login);
-
 //serve static files in the public directory
 app.use(express.static('public'));
 
@@ -47,7 +36,6 @@ app.use(express.static('public'));
 app.get('/', function (req, res) {
 	res.send('Hello world, I am a chat bot')
 })
-
 
 // for Facebook verification
 app.get('/webhook/', apiAiModule.webhookGet);
