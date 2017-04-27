8ce00713022082fefbf3ffcbd4c6a87bb2700242
lirl <lironlavon@gmail.com>
2017-04-27 22:17:07 +0300

    remove unused lib

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const bodyParser = require('body-parser');
 const apiAiModule = require('./apiaiModule');
 
 const passport = require('passport');
-const createSendToken = require('./services/jwt.js');
 const facebookAuth = require('./services/facebookAuth.js');
 const localStrategy = require('./services/localStrategy.js');
 //const cognitiveServices = require('cognitive-services');
